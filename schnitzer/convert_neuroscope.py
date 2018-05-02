81726b1bc6e62b8304d1a6acf3bc2b95a8ea167a
bendichter <ben.dichter@gmail.com>
2018-05-02 09:56:32 -0600

    update schnitzer

--- schnitzer/convert_neuroscope.py
+++ schnitzer/convert_neuroscope.py
@@ -1,8 +1,75 @@
 import os
 import numpy as np
+from neuroscope import get_lfp_sampling_rate, get_channel_groups
+
+from pynwb.ecephys import ElectricalSeries, LFP
+from pynwb import NWBFile, NWBHDF5IO, TimeSeries
+from dateutil.parser import parse as parse_date
+
+"""
+Time simply increments by 1
+"""
 
 fpath_base = '/Users/bendichter/Desktop/Schnitzer/data/test1_171207_181558'
+this_dir = fpath_base.split('/')[-1]
+name, day, time = this_dir.split('_')
 
-amp_fpath = os.path.join(fpath_base, 'amplifier.dat')
+session_start_time = parse_date(day, yearfirst=True)
 
-amp_data = np.fromfile(amp_fpath, dtype=np.int16).reshape(-1, 32)
+amp_xml_path = os.path.join(fpath_base, 'amplifier.xml')
+amp_fs = get_lfp_sampling_rate(amp_xml_path)
+nchannels = len(get_channel_groups(amp_xml_path)[0])
+
+
+datas = ['amplifier', 'time', 'auxiliary', 'supply']
+data_fpaths = {name: os.path.join(fpath_base, name + '.dat')
+               for name in datas}
+
+amp_data = np.fromfile(data_fpaths['amplifier'], dtype=np.int16).reshape(-1, nchannels)
+time_data = np.fromfile(data_fpaths['time'], dtype=np.int32)
+supply_data = np.fromfile(data_fpaths['supply'], dtype=np.int16)
+ntt = len(amp_data)
+aux_data = np.fromfile(data_fpaths['auxiliary'], dtype=np.int16).reshape(ntt, -1)
+
+nwbfile = NWBFile(session_start_time=session_start_time, identifier=this_dir,
+                  source=this_dir, session_description='unknown')
+
+device = nwbfile.create_device(name='all_channels_device', source=' ')
+group = nwbfile.create_electrode_group(name='all_channels_group',
+                                       source=' ',
+                                       description='all channels',
+                                       device=device,
+                                       location='unknown')
+
+for i in range(nchannels):
+    nwbfile.add_electrode(i,
+                          np.nan, np.nan, np.nan,  # position
+                          imp=np.nan,
+                          location='unknown',
+                          filtering='unknown',
+                          description='electrode {}'.format(i),
+                          group=group)
+electrode_table_region = nwbfile.create_electrode_table_region(
+    list(range(nchannels)), 'all electrodes')
+
+
+electrical_series = ElectricalSeries(data=amp_data, starting_time=0.0,
+                                     rate=amp_fs, units='unknown',
+                                     electrodes=electrode_table_region,
+                                     name='amp_data',
+                                     source=data_fpaths['amplifier'])
+nwbfile.add_acquisition(
+    LFP(name='amp_data', source=data_fpaths['amplifier'],
+        electrical_series=electrical_series))
+
+nwbfile.add_acquisition(
+    TimeSeries('auxiliary', source=data_fpaths['auxiliary'], data=aux_data,
+               starting_time=0.0, rate=amp_fs))
+nwbfile.add_acquisition(
+    TimeSeries('supply', source=data_fpaths['supply'], data=supply_data,
+               starting_time=0.0, rate=amp_fs))
+
+out_fname = this_dir + '.nwb'
+with NWBHDF5IO(out_fname, 'w') as io:
+    io.write(nwbfile)
+
--- schnitzer/example_data.py
+++ schnitzer/example_data.py
@@ -1,11 +1,12 @@
 import os
 
 from pynwb.behavior import Position
-from pynwb.ophys import Fluorescence
+from pynwb.ophys import Fluorescence, RoiResponseSeries, ROITable
 from pynwb import NWBHDF5IO, NWBFile
 from h5py import File
 import numpy as np
 from datetime import datetime
+
 
 base_dir = '/Users/bendichter/Desktop/Schnitzer/data/Example Data'
 
@@ -31,6 +32,22 @@ module_pos.add_container(pos)
 fluorescence_path = os.path.join(base_dir, 'm655_D11_S1.hdf5')
 with File(fluorescence_path, 'r') as file:
     fluorescence_data = file['Data']['Images'][:]
+
+data_names = ['cellImages', 'cellTraces', 'centroids', 'eventTimes']
+mat_data = {}
+mat_output_fpath = os.path.join(base_dir, 'm655_D11_S1_output.mat')
+with File(mat_output_fpath, 'r') as file:
+    for name in data_names:
+        mat_data[name] = file['output'][name][:]
+
+roi_table = ROITable()
+for i, image in enumerate(mat_data['cellImages']):
+    roi_table.add_row(name=str(i), pixel_mask=image, image_mask=image)
+
+
+
+#RoiResponseSeries(data=mat_data['cellTraces'], name='all cells',
+#                  source=mat_output_fpath, unit='unknown', rois='huh?')
 
 #  not working
 nwbfile.add_acquisition(Fluorescence(source=' ', data=fluorescence_data))

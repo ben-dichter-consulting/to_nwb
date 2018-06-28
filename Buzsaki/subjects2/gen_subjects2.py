830aac30b04cee547116aa0c5129f33d9d9f0247
bendichter <ben.dichter@gmail.com>
2018-06-27 21:18:37 -0400

    make extensions for Peter's meta-data tables

--- Buzsaki/subjects2/gen_subjects2.py
+++ Buzsaki/subjects2/gen_subjects2.py
@@ -7,51 +7,124 @@ name = 'subject2'
 ns_path = name + ".namespace.yaml"
 ext_source = name + ".extensions.yaml"
 
-virus_injection = NWBGroupSpec(neurodata_type_def='VirusInjection',
-                               name='virus_injection', quantity='?',
-                               doc='notes about surgery that includes virus injection',
-                               datasets=[NWBDatasetSpec(name='coordinates', doc='(AP, ML, DV) of virus injection',
-                                                        dtype='float', shape=(3,))],
-                               attributes=[NWBAttributeSpec(name='virus_id', doc='id of virus', dtype='text'),
-                                           NWBAttributeSpec(name='volume', doc='volume of injecting in microliters',
-                                                            dtype='float')])
+surgery = NWBGroupSpec(
+    neurodata_type_def='Surgery',
+    name='surgery', doc='relevant data for surgery', quantity='?',
+    datasets=[NWBDatasetSpec(name='devices', quantity='?',
+                             doc='links to implanted/explanted devices',
+                             dtype=RefSpec('Device', 'object'))],
+    attributes=[
+        NWBAttributeSpec(name='start_datetime', doc='datetime in ISO 8601', dtype='text', required=False),
+        NWBAttributeSpec(name='end_datetime', doc='datetime in ISO 8601', dtype='text', required=False),
+        NWBAttributeSpec(name='weight', required=False, dtype='text',
+                         doc='Weight at time of experiment, at time of surgery and at other '
+                             'important times'),
+        NWBAttributeSpec(name='notes', doc='notes and complications', dtype='text', required=False),
+        NWBAttributeSpec(name='anesthesia', doc='anesthesia', dtype='text', required=False),
+        NWBAttributeSpec(name='analgesics', doc='analgesics', dtype='text', required=False),
+        NWBAttributeSpec(name='antibiotics', doc='antibiotics', dtype='text', required=False),
+        NWBAttributeSpec(name='target_anatomy', doc='target anatomy', dtype='text', required=False),
+        NWBAttributeSpec(name='room', doc='place where the surgery took place', dtype='text',
+                         required=False),
+        NWBAttributeSpec(name='type', doc='"chronic" or "acute"', dtype='text', required=False)
+    ])
 
-surgery = NWBGroupSpec(neurodata_type_def='Surgery',
-                       name='Surgery', doc='relevant data for surgery', quantity='?',
-                       datasets=[NWBDatasetSpec(name='devices', doc='links to devices', quantity='?',
-                                                dtype=RefSpec('Device', 'object'))],
-                       groups=[virus_injection],
-                       attributes=[
-                           NWBAttributeSpec(name='date', doc='date in ISO 8601', dtype='text', required=False),
-                           NWBAttributeSpec(name='notes', doc='notes', dtype='text', required=False),
-                           NWBAttributeSpec(name='pharmacology', doc='pharmacology', dtype='text', required=False),
-                           NWBAttributeSpec(name='target_anatomy', doc='target anatomy', dtype='text', required=False)
-                       ])
+virus_injection = NWBGroupSpec(
+    neurodata_type_inc='Surgery',
+    neurodata_type_def='VirusInjection',
+    name='VirusInjection', quantity='?',
+    doc='notes about surgery that includes virus injection',
+    datasets=[NWBDatasetSpec(name='coordinates', doc='(AP, ML, DV) of virus injection',
+                             dtype='float', shape=(3,))],
+    attributes=[
+        NWBAttributeSpec(name='virus', doc='type of virus', dtype='text'),
+        NWBAttributeSpec(name='volume', doc='volume of injecting in nL', dtype='float'),
+        NWBAttributeSpec(name='rate', doc='rate of injection (nL/s)',
+                         dtype='float', required=False),
+        NWBAttributeSpec(name='scheme', doc='scheme of injection', dtype='text', required=False)])
 
-subject = NWBGroupSpec(neurodata_type_def='Subject2',
-                       name='Subject2',
-                       doc='information about subject',
-                       groups=[surgery],
-                       attributes=[NWBAttributeSpec(name='genotype', dtype='text', doc='genotype', required=False),
-                                   NWBAttributeSpec(name='date_of_birth', dtype='text', doc='ISO 8601', required=False),
-                                   NWBAttributeSpec(name='age', doc='age of subject. No specific format enforced.',
-                                                    dtype='text', required=False),
-                                   NWBAttributeSpec(name='sex', quantity='?', dtype='text',
-                                                    doc='Sex of subject. Options: "M": male, "F": female, "O": other, '
-                                                        '"U": unknown', required=True),
-                                   NWBAttributeSpec(name='gender', dtype='text', required=False,
-                                                    doc='Gender of subject if different from sex.'),
-                                   NWBAttributeSpec(name='species', doc='Species of subject', dtype='text',
-                                                    quantity='?'),
-                                   NWBAttributeSpec(name='weight', required=False, dtype='text',
-                                                    doc='Weight at time of experiment, at time of surgery and at other '
-                                                        'important times'),
-                                   NWBAttributeSpec(name='subject_id', required=True, dtype='text',
-                                                    doc='ID of animal/person used/participating in experiment (lab '
-                                                        'convention)'),
-                                   NWBAttributeSpec(name='weight', required=False, dtype='text',
-                                                    doc='Weight at time of experiment, at time of surgery and at other '
-                                                        'important times')])
+histology = NWBGroupSpec(
+    neurodata_type_def='Histology',
+    name='Histology',
+    doc='information about histology of subject',
+    quantity='?',
+    attributes=[
+        NWBAttributeSpec(name='file_name', doc='filename of histology images', dtype='text'),
+        NWBAttributeSpec(name='file_name_ext', doc='filename extension', dtype='text'),
+        NWBAttributeSpec(name='imaging_technique',
+                         doc='histology imaging technique (e.g. widefield, confocal, etc.)',
+                         dtype='text'),
+        NWBAttributeSpec(name='slice_plane', doc='[Coronal, Sagital, Transverse, Other]',
+                         required=False, dtype='text'),
+        NWBAttributeSpec(name='slice_thickness', docs='thickness of slice (um)', dtype='float',
+                         required=False),
+        NWBAttributeSpec(name='location_along_axis', docs='Axis orthogal to SlicePlane (mm)',
+                         dtype='float', required=False),
+        NWBAttributeSpec(name='brain_region_target', docs='Allen Institute acronym',
+                         dtype='text', required=False),
+        NWBAttributeSpec(name='stainings', docs='stainings', dtype='text', required=False),
+        NWBAttributeSpec(name='light_source', docs='wavelength of light source in nm',
+                         dtype='float', required=False),
+        NWBAttributeSpec(name='image_scale', docs='scale of image (pixels/100um)', dtype='float',
+                         required=False),
+        NWBAttributeSpec(name='scale_bar', docs='size of image scale bar (um)', dtype='float',
+                         required=False),
+        NWBAttributeSpec(name='post_processing', docs='[Z-stacked, Stiched]', dtype='text',
+                         required=False),
+        NWBAttributeSpec(name='user', docs='person involved', dtype='text', required=False),
+        NWBAttributeSpec(name='notes', docs='anything else', dtype='text', required=False)
+    ])
+
+
+subject = NWBGroupSpec(
+    neurodata_type_def='Subject2',
+    name='Subject2',
+    doc='information about subject',
+    groups=[surgery, histology],
+    attributes=[
+        NWBAttributeSpec(name='subject_id', required=True, dtype='text',
+                         doc='ID of subject (lab convention)'),
+        NWBAttributeSpec(
+            name='sex', quantity='?', dtype='text', required=True,
+            doc='Sex of subject. Options: "M": male, "F": female, "O": other, "U": unknown'),
+        NWBAttributeSpec(name='species', doc='Species of subject', dtype='text',
+                         quantity='?'),
+        NWBAttributeSpec(name='strain', dtype='text', doc='strain of animal',
+                         required=False),
+        NWBAttributeSpec(name='genetic_line', dtype='text', doc='genetic line of animal',
+                         required=False),
+        NWBAttributeSpec(name='date_of_birth', dtype='text', doc='in ISO 8601 format',
+                         required=False),
+        NWBAttributeSpec(name='date_of_death', dtype='text', doc='in ISO 8601 format',
+                         required=False),
+        NWBAttributeSpec(name='age', doc='age of subject. No specific format enforced.',
+                         dtype='text', required=False),
+        NWBAttributeSpec(name='gender', dtype='text', required=False,
+                         doc='Gender of subject if different from sex.'),
+        NWBAttributeSpec(name='earmark', dtype='text', required=False,
+                         doc='Earmark of subject'),
+        NWBAttributeSpec(name='weight', required=False, dtype='text',
+                         doc='Weight at time of experiment, at time of surgery and at other '
+                             'important times')])
+
+OpticalFiber = NWBGroupSpec(
+    neurodata_type_inc='Device',
+    neurodata_type_def='OpticalFiber',
+    name='OpticalFiber',
+    attributes=[
+        NWBAttributeSpec(name='type', doc='model', dtype='text', required=False),
+        NWBAttributeSpec(name='core_diameter', doc='in um', dtype='float', required=False),
+        NWBAttributeSpec(name='outer_diameter', doc='in um', dtype='float', required=False),
+        NWBAttributeSpec(name='microdrive', doc='whether a microdrive was used',
+                         dtype='logical'),
+        NWBAttributeSpec(name='microdrive_lead', doc='um/turn', dtype='float', required=False),
+        NWBAttributeSpec(name='microdrive_id', doc=' ', dtype='int', required=False)
+    ]
+)
+
+# for manipulation
+#NWBAttributeSpec(name='rotation', doc='rotation of microdrive (degrees)',
+#                 dtype='float', required=False),
 
 
 ns_builder = NWBNamespaceBuilder(name + ' extensions', name)
@@ -83,3 +156,5 @@ Subject(genotype='mouse1', species='mouse', sex='U', subject_id='007',
                 volume=0.3
             )
         ))
+
+

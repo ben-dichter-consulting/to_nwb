3d1566813387c0c483d9eb5f2f7aea94215e4b4e
bendichter <ben.dichter@gmail.com>
2018-09-03 21:05:34 -0700

    update ecog extension

--- to_nwb/extensions/ecog/ecog.py
+++ to_nwb/extensions/ecog/ecog.py
@@ -1,7 +1,7 @@
 import os
 
 from pynwb import load_namespaces
-from ..auto_class import get_class
+from ..auto_class import get_class, get_multi_container
 
 filepath = os.path.realpath(__file__)
 basedir = os.path.split(filepath)[0]
@@ -10,4 +10,6 @@ name = 'ecog'
 load_namespaces(os.path.join(basedir, name + '.namespace.yaml'))
 
 
-CorticalSurface = get_class(name, 'CorticalSurface')
+Surface = get_class(name, 'Surface')
+
+CorticalSurfaces = get_multi_container(name, 'CorticalSurfaces', Surface)
--- to_nwb/extensions/ecog/gen_ecog_yaml.py
+++ to_nwb/extensions/ecog/gen_ecog_yaml.py
@@ -4,15 +4,22 @@ name = 'ecog'
 ns_path = name + ".namespace.yaml"
 ext_source = name + ".extensions.yaml"
 
-surface = NWBGroupSpec(doc='brain cortical surface',
+surface = NWBGroupSpec(neurodata_type_def='Surface',
+                       neurodata_type_inc='NWBDataInterface',
+                       quantity='+',
+                       doc='brain cortical surface',
                        datasets=[  # set Faces and Vertices as elements of the Surfaces neurodata_type
                            NWBDatasetSpec(doc='faces for surface, indexes vertices', shape=(None, 3),
                                           name='faces', dtype='uint', dims=('face_number', 'vertex_index')),
                            NWBDatasetSpec(doc='vertices for surface, points in 3D space', shape=(None, 3),
-                                          name='vertices', dtype='float', dims=('vertex_number', 'xyz'))],
-                       neurodata_type_def='CorticalSurface',
-                       neurodata_type_inc='NWBDataInterface')
+                                          name='vertices', dtype='float', dims=('vertex_number', 'xyz'))])
+
+surfaces = NWBGroupSpec(neurodata_type_def='CorticalSurfaces',
+                        neurodata_type_inc='NWBDataInterface',
+                        name='cortical_surfaces',
+                        doc='triverts for cortical surfaces', quantity='?',
+                        groups=[surface])
 
 ns_builder = NWBNamespaceBuilder(name + ' extensions', name)
-ns_builder.add_spec(ext_source, surface)
+ns_builder.add_spec(ext_source, surfaces)
 ns_builder.export(ns_path)

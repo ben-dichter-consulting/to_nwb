bef0f41bb90e23e226a458daf21c8c611d40cd97
bendichter <ben.dichter@gmail.com>
2018-09-04 12:48:33 -0700

    bring general up to speed

--- to_nwb/extensions/general/gen_yaml.py
+++ to_nwb/extensions/general/gen_yaml.py
@@ -1,9 +1,9 @@
 from pynwb.spec import NWBDatasetSpec, NWBNamespaceBuilder, NWBGroupSpec, \
     NWBAttributeSpec
 
-name = 'general'
-ns_path = name + '.namespace.yaml'
-ext_source = name + '.extensions.yaml'
+namespace = 'general'
+ns_path = namespace + '.namespace.yaml'
+ext_source = namespace + '.extensions.yaml'
 
 values = NWBAttributeSpec(name='values',
                           dtype='text',
@@ -13,13 +13,19 @@ values = NWBAttributeSpec(name='values',
 cat_cell_info = NWBGroupSpec(
     neurodata_type_def='CatCellInfo',
     doc='Categorical Cell Info',
+    attributes=[NWBAttributeSpec(
+        name='help',
+        doc='help',
+        dtype='text',
+        value='Categorical information about cells. For most cases the units tables is more appropriate. This '
+              'structure can be used if you need multiple entries per cell')],
     datasets=[
         NWBDatasetSpec(doc='global id for neuron',
-                       shape=(None, 1),
+                       shape=(None,),
                        name='cell_index', dtype='int', quantity='?'),
         NWBDatasetSpec(name='indices',
                        doc='list of indices for values',
-                       shape=(None, 1), dtype='int',
+                       shape=(None,), dtype='int',
                        attributes=[values])],
     neurodata_type_inc='NWBDataInterface')
 
@@ -32,7 +38,9 @@ cat_timeseries = NWBGroupSpec(
                              doc='timeseries of indicies for values',
                              attributes=[values])])
 
-ns_builder = NWBNamespaceBuilder(name + ' extensions', name)
+ns_builder = NWBNamespaceBuilder(doc=namespace + ' extensions', name=namespace,
+                                 version='1.0', author='Ben Dichter',
+                                 contact='bendichter@gmail.com')
 for spec in (cat_cell_info, cat_timeseries):
     ns_builder.add_spec(ext_source, spec)
 ns_builder.export(ns_path)

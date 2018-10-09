bfd3f0608b2db66c927d382fab0c5cb1fe76804d
bendichter <ben.dichter@gmail.com>
2018-10-08 17:03:57 -0700

    update for ElectrodeTable as DynamicTable

--- to_nwb/Buzsaki/convert_yuta.py
+++ to_nwb/Buzsaki/convert_yuta.py
@@ -21,8 +21,6 @@ from to_nwb import neuroscope as ns
 
 from ephys_analysis.analysis import filter_lfp, hilbert_lfp
 
-WRITE_ALL_LFPS = False
-
 
 def parse_states(fpath):
 
@@ -48,14 +46,14 @@ def parse_states(fpath):
     return states, state_times
 
 
-def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMouse41-150903',
+def yuta2nwb(session_path='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMouse41-150903',
              subject_xls='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YM41 exp_sheet.xlsx',
              stub=False):
 
-    fpath_base, fname = os.path.split(fpath)
-    identifier = fname
+    fpath_base, session_name = os.path.split(session_path)
+    identifier = session_name
 
-    subject_id, date_text = fname.split('-')
+    subject_id, date_text = session_name.split('-')
     session_start_time = dateparse(date_text, yearfirst=True)
 
     df = pd.read_excel(subject_xls)
@@ -70,14 +68,14 @@ def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMous
                       genotype=subject_data['genotype'],
                       species='mouse', source='source')
 
-    source = fname
+    source = session_name
     nwbfile = NWBFile(source=source,
                       session_description='mouse in open exploration and theta maze',
                       identifier=identifier,
                       session_start_time=session_start_time,
                       file_create_date=datetime.now(),
                       experimenter='Yuta Senzai',
-                      session_id=fname,
+                      session_id=session_name,
                       institution='NYU',
                       lab='Buzsaki',
                       subject=subject,
@@ -85,19 +83,16 @@ def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMous
 
     all_ts = []
 
-    xml_filepath = os.path.join(fpath, fname + '.xml')
+    xml_filepath = os.path.join(session_path, session_name + '.xml')
 
-    channel_groups = ns.get_channel_groups(xml_filepath)
     shank_channels = ns.get_shank_channels(xml_filepath)
-    nshanks = len(shank_channels)
     all_shank_channels = np.concatenate(shank_channels)
-    nchannels = sum(len(x) for x in channel_groups)
     lfp_fs = ns.get_lfp_sampling_rate(xml_filepath)
 
     lfp_channel = 0  # value taken from Yuta's spreadsheet
 
     print('reading raw position data...', end='', flush=True)
-    pos_df = ns.get_position_data(fpath, fname)
+    pos_df = ns.get_position_data(session_path)
     print('done.')
 
     print('setting up raw position data...', end='', flush=True)
@@ -122,24 +117,24 @@ def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMous
     print('done.')
 
     print('setting up electrodes...', end='', flush=True)
+    nwbfile.add_electrode_column('electrode_description',
+                                 description='description of electrode description???')
     # shank electrodes
     electrode_counter = 0
     for shankn, channels in enumerate(shank_channels):
         device_name = 'shank{}'.format(shankn)
-        device = nwbfile.create_device(device_name, fname + '.xml')
+        device = nwbfile.create_device(device_name, session_name + '.xml')
         electrode_group = nwbfile.create_electrode_group(
             name=device_name + '_electrodes',
-            source=fname + '.xml',
+            source=session_name + '.xml',
             description=device_name,
             device=device,
             location='unknown')
         for channel in channels:
-            nwbfile.add_electrode(channel,
-                                  np.nan, np.nan, np.nan,  # position?
-                                  imp=np.nan,
-                                  location='unknown',
+            nwbfile.add_electrode(id=channel, x=np.nan, y=np.nan, z=np.nan,  # position?
+                                  imp=np.nan, location='unknown',
                                   filtering='unknown',
-                                  description='electrode {} of shank {}, channel {}'.format(
+                                  electrode_description='electrode {} of shank {}, channel {}'.format(
                                       electrode_counter, shankn, channel),
                                   group=electrode_group)
 
@@ -151,10 +146,10 @@ def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMous
 
     # special electrodes
     device_name = 'special'
-    device = nwbfile.create_device(device_name, fname + '.xml')
+    device = nwbfile.create_device(device_name, session_name + '.xml')
     electrode_group = nwbfile.create_electrode_group(
         name=device_name + '_electrodes',
-        source=fname + '.xml',
+        source=session_name + '.xml',
         description=device_name,
         device=device,
         location='unknown')
@@ -163,12 +158,10 @@ def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMous
                               'ch_entL': 72, 'ch_entR': 71, 'ch_SsolL': 73,
                               'ch_SsolR': 70}
     for name, num in special_electrode_dict.items():
-        nwbfile.add_electrode(num,
-                              np.nan, np.nan, np.nan,
-                              imp=np.nan,
-                              location='unknown',
+        nwbfile.add_electrode(id=num, x=np.nan, y=np.nan, z=np.nan,
+                              imp=np.nan, location='unknown',
                               filtering='unknown',
-                              description=name,
+                              electrode_description=name,
                               group=electrode_group)
         nwbfile.create_electrode_table_region([electrode_counter], name)
         electrode_counter += 1
@@ -181,7 +174,7 @@ def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMous
     print('reading LFPs...', end='', flush=True)
 
     if not stub:
-        lfp_file = os.path.join(fpath, fname + '.eeg')
+        lfp_file = os.path.join(session_path, session_name + '.eeg')
         all_channels = np.fromfile(lfp_file, dtype=np.int16).reshape(-1, 80)
         all_channels_lfp = all_channels[:, all_shank_channels]
 
@@ -201,7 +194,6 @@ def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMous
         data,
         all_table_region,
         conversion=np.nan,
-        starting_time=0.0,
         rate=lfp_fs,
         resolution=np.nan)
     all_ts.append(all_lfp_electrical_series)
@@ -210,14 +202,9 @@ def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMous
     print('done.')
 
     electrical_series = ElectricalSeries(
-        'reference_lfp',
-        'signal used as the reference lfp',
+        'reference_lfp', 'signal used as the reference lfp',
         H5DataIO(all_channels[:, lfp_channel], compression='gzip'),
-        lfp_table_region,
-        conversion=np.nan,
-        starting_time=0.0,
-        rate=lfp_fs,
-        resolution=np.nan)
+        lfp_table_region, conversion=np.nan, rate=lfp_fs, resolution=np.nan)
 
     nwbfile.add_acquisition(LFP(source='source', name='reference_lfp',
                                 electrical_series=electrical_series))
@@ -233,7 +220,7 @@ def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMous
                                                        description=source)
     for label in task_types:
         print('loading normalized position data for ' + label + '...', end='', flush=True)
-        file = os.path.join(fpath, fname + '__' + label)
+        file = os.path.join(session_path, session_name + '__' + label)
 
         matin = loadmat(file)
         tt = matin['twhl_norm'][:, 0]
@@ -241,13 +228,12 @@ def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMous
 
         exp_times = find_discontinuities(tt)
 
-        spatial_series_object = SpatialSeries(name=label + '_spatial_series',
-                                              source='position sensor0',
-                                              data=H5DataIO(pos_data, compression='gzip'),
-                                              reference_frame='unknown',
-                                              conversion=np.nan,
-                                              resolution=np.nan,
-                                              timestamps=H5DataIO(tt, compression='gzip'))
+        spatial_series_object = SpatialSeries(
+            name=label + '_spatial_series', source='position sensor0',
+            data=H5DataIO(pos_data, compression='gzip'),
+            reference_frame='unknown', conversion=np.nan, resolution=np.nan,
+            timestamps=H5DataIO(tt, compression='gzip'))
+
         pos_obj = Position(source=source,
                            spatial_series=spatial_series_object,
                            name=label + '_position')
@@ -278,9 +264,9 @@ def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMous
         10: 'positive negative waveform unit'
     }
 
-    region_dict = {3: 'CA3', 4: 'DG'}
+    # regions: 3: 'CA3', 4: 'DG'
 
-    this_file = matin.fname == fname
+    this_file = matin.fname == session_name
     celltype_ids = matin.fineCellType.ravel()[this_file]
     region_ids = matin.region.ravel()[this_file]
     unit_ids = matin.unitID.ravel()[this_file]
@@ -304,17 +290,17 @@ def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMous
     for unit_id, celltype, shank in zip(unit_ids, celltype_names, shanks):
         nwbfile.add_unit({'id': unit_id, 'cell_type': celltype, 'shank': shank})
 
-    ut_obj = ns.build_unit_times(fpath, fname, unit_ids=unit_ids)
+    ut_obj = ns.build_unit_times(session_path, unit_ids=unit_ids)
 
-    module_cellular = nwbfile.create_processing_module('cellular', source=source,
-                                                       description=source)
+    module_cellular = nwbfile.create_processing_module(
+        'cellular', source=source, description=source)
 
     module_cellular.add_container(ut_obj)
 
-    trialdata_path = os.path.join(fpath, fname + '__EightMazeRun.mat')
+    trialdata_path = os.path.join(session_path, session_name + '__EightMazeRun.mat')
     trials_data = loadmat(trialdata_path)['EightMazeRun']
 
-    trialdatainfo_path = os.path.join(fpath, fname + '__EightMazeRunInfo.mat')
+    trialdatainfo_path = os.path.join(session_path, session_name + '__EightMazeRunInfo.mat')
     trialdatainfo = [x[0] for x in loadmat(trialdatainfo_path)['EightMazeRunInfo'][0]]
 
     features = trialdatainfo[:7]
@@ -324,23 +310,24 @@ def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMous
     for trial_data in trials_data:
         nwbfile.add_trial({lab: dat for lab, dat in zip(features, trial_data[:7])})
 
-    mono_syn_fpath = os.path.join(fpath, fname+'-MonoSynConvClick.mat')
+    mono_syn_fpath = os.path.join(session_path, session_name+'-MonoSynConvClick.mat')
 
     matin = loadmat(mono_syn_fpath)
     exc = matin['FinalExcMonoSynID']
     inh = matin['FinalInhMonoSynID']
 
-    exc_obj = CatCellInfo(name='excitatory_connections', source='YutaMouse41-150903-MonoSynConvClick.mat',
+    exc_obj = CatCellInfo(name='excitatory_connections', source=session_name+'-MonoSynConvClick.mat',
                           values=[], cell_index=exc[:, 0] - 1, indices=exc[:, 1] - 1)
     module_cellular.add_container(exc_obj)
-    inh_obj = CatCellInfo(name='inhibitory_connections', source='YutaMouse41-150903-MonoSynConvClick.mat',
+    inh_obj = CatCellInfo(name='inhibitory_connections', source=session_name+'-MonoSynConvClick.mat',
                           values=[], cell_index=inh[:, 0] - 1, indices=inh[:, 1] - 1)
     module_cellular.add_container(inh_obj)
 
-    sleep_state_fpath = os.path.join(fpath, fname+'--StatePeriod.mat')
+    sleep_state_fpath = os.path.join(session_path, session_name+'--StatePeriod.mat')
     matin = loadmat(sleep_state_fpath)['StatePeriod']
 
-    table = DynamicTable(name='states', source='source', description='sleep states of animal')
+    table = DynamicTable(name='states', source='source',
+                         description='sleep states of animal')
     table.add_column(name='start', description='start time')
     table.add_column(name='end', description='end time')
     table.add_column(name='state', description='sleep state')
@@ -367,7 +354,9 @@ def yuta2nwb(fpath='/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/YutaMous
 
         module_lfp.add_container(time_series)
 
-    out_fname = '/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/' + fname + '_mini.nwb'
+    out_fname = '/Users/bendichter/Desktop/Buzsaki/SenzaiBuzsaki2017/' + session_name + '.nwb'
+    if stub:
+        out_fname = out_fname[:-4] + '_stub.nwb'
     print('writing NWB file...', end='', flush=True)
     with NWBHDF5IO(out_fname, mode='w') as io:
         io.write(nwbfile)

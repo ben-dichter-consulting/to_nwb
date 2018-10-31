fc5a1de6c1a00941dbde4b271e0449a6c1fdfb95
bendichter <ben.dichter@gmail.com>
2018-10-31 18:08:45 -0400

    rmv source

--- to_nwb/schnitzer/example_data.py
+++ to_nwb/schnitzer/example_data.py
@@ -12,10 +12,10 @@ from datetime import datetime
 
 base_dir = '/Users/bendichter/Desktop/Schnitzer/data/Example Data'
 
-nwbfile = NWBFile(session_start_time=datetime(1900, 1, 1), source=base_dir,
+nwbfile = NWBFile(session_start_time=datetime(1900, 1, 1),
                   session_description=' ', identifier='m655_D11_S1')
 
-nwbfile.add_acquisition(ImageSeries(name='video', source='m655_D11_S1.avi',
+nwbfile.add_acquisition(ImageSeries(name='video',
                                     format='external',
                                     external_file=['m655_D11_S1.avi'],
                                     starting_time=0.0, rate=np.nan,
@@ -27,16 +27,13 @@ pos_path = os.path.join(base_dir, centroid_fname)
 
 with File(pos_path, 'r') as file:
     pos_data = np.array(file['c']).T
-spatial_series = SpatialSeries(name='position', source='source',
-                               data=pos_data, starting_time=0.0, rate=5.0,
-                               units='unknown', reference_frame='unknown')
+spatial_series = SpatialSeries(name='position', data=pos_data, starting_time=0.0,
+                               rate=5.0, units='unknown', reference_frame='unknown')
 
-pos = Position(name='position', source=centroid_fname,
-               spatial_series=spatial_series,
-               starting_time=0.0, rate=np.nan)
+pos = Position(name='position', spatial_series=spatial_series, starting_time=0.0,
+               rate=np.nan)
 
-module_pos = nwbfile.create_processing_module(name='position', source=' ',
-                                              description='position')
+module_pos = nwbfile.create_processing_module(name='position', description='position')
 
 module_pos.add_container(pos)
 
@@ -48,11 +45,10 @@ with File(mat_output_fpath, 'r') as file:
         mat_data[name] = file['output'][name][:]
 
 
-optical_channel = OpticalChannel('my channel', 'source', 'description',
-                                 np.nan)
+optical_channel = OpticalChannel('my channel', 'description', np.nan)
 
 imaging_plane = nwbfile.create_imaging_plane(
-    'my_imgpln', 'm655_D11_S1_output.mat', optical_channel,
+    'my_imgpln', optical_channel,
     'description', 'imaging_device_1',
     600., '2.718', 'GFP', 'my favorite brain location',
     [], 4.0, 'manifold unit', 'A frame to refer to')
@@ -60,18 +56,15 @@ imaging_plane = nwbfile.create_imaging_plane(
 
 images_path = os.path.join(base_dir, 'm655_D11_S1.hdf5')
 with File(images_path, 'r') as file:
-    image_series = TwoPhotonSeries(name='test_iS', source='Ca2+ imaging example', dimension=[2],
-                                   data=file['Data']['Images'][:], imaging_plane=imaging_plane,
-                                   starting_frame=[0], starting_time=0.0, rate=5.0, scan_line_rate=np.nan,
-                                   pmt_gain=np.nan)
+    image_series = TwoPhotonSeries(name='test_iS', dimension=[2], data=file['Data']['Images'][:],
+                                   imaging_plane=imaging_plane, starting_frame=[0], starting_time=0.0, rate=5.0,
+                                   scan_line_rate=np.nan, pmt_gain=np.nan)
 nwbfile.add_acquisition(image_series)
 
 
-mod = nwbfile.create_processing_module('rois', 'Ca2+ imaging example', 'example data module')
-img_seg = ImageSegmentation('Ca2+ imaging example')
-ps = img_seg.create_plane_segmentation('Ca2+ imaging example',
-                                       'output from segmenting my favorite imaging plane',
-                                       imaging_plane, 'my_planeseg', image_series)
+mod = nwbfile.create_processing_module('rois', 'example data module')
+img_seg = ImageSegmentation()
+ps = img_seg.create_plane_segmentation('Ca2+ imaging example', imaging_plane, 'my_planeseg', image_series)
 mod.add_data_interface(img_seg)
 
 
@@ -82,9 +75,9 @@ for i, img_mask in enumerate(zip(mat_data['cellImages'])):
 
 region = ps.create_roi_table_region('all', region=list(range(len(mat_data['cellImages']))))
 
-roi_response = RoiResponseSeries('RoiResponseSeries', 'source', mat_data['cellTraces'],
+roi_response = RoiResponseSeries('RoiResponseSeries', mat_data['cellTraces'],
                                  'lumens?', region, rate=5.0, starting_time=0.0)
-fl = Fluorescence('source', roi_response_series=roi_response)
+fl = Fluorescence(roi_response_series=roi_response)
 mod.add_data_interface(fl)
 
 fname_out = 'm655_D11_S1.nwb'

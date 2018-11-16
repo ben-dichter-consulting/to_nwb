2b38cc6481af0347dd2fb9b5dbe317740f0101c3
bendichter <ben.dichter@gmail.com>
2018-11-16 14:44:19 -0500

    handle raw data stored with HDF5

--- to_nwb/tdt.py
+++ to_nwb/tdt.py
@@ -1,17 +1,38 @@
 from scipy.io import loadmat
 import numpy as np
+from h5py import File
 
 
 def load_wavs(raw_path):
+    try:
+        return load_wavs_mat(raw_path)
+    except:
+        return load_wavs_h5py(raw_path)
+
+
+def load_wavs_mat(raw_path):
     out = []
     raw_matin = loadmat(raw_path, struct_as_record=True)
-    wav_streams = raw_matin['data']['streams'][0][0]
-    stream_names = wav_streams.dtype.names
-    wav_stream_names = sorted([x for x in stream_names if x[:3] == 'Wav'])
+    streams = raw_matin['data']['streams'][0][0]
+    wav_stream_names = sorted([x for x in streams.dtype.names if x[:3] == 'Wav'])
     for stream in wav_stream_names:
-        out.append(wav_streams[stream][0, 0]['data'][0, 0])
-    fs = wav_streams[stream][0, 0]['fs'][0, 0][0, 0]
-    return fs, np.vstack(out).T
+        out.append(streams[stream][0, 0]['data'][0, 0])
+    fs = streams[stream][0, 0]['fs'][0, 0][0, 0]
+    data = np.vstack(out).T
+    return fs, data
+
+
+def load_wavs_h5py(raw_path):
+
+    out = []
+    with File(raw_path) as f:
+        streams = f['data']['streams']
+        wav_stream_names = sorted([x for x in streams.keys() if x[:3] == 'Wav'])
+        for stream in wav_stream_names:
+            out.append(streams[stream]['data'][:])
+        fs = streams[stream]['fs']
+    data = np.hstack(out)
+    return fs, data
 
 
 def load_anin(raw_path, num=None):

d57d1e12a675901d047e62cdb5282fe1519d25f8
bendichter <ben.dichter@gmail.com>
2018-11-26 16:28:04 -0500

    simplify syntax

--- to_nwb/Buzsaki/convert_neuroscope.py
+++ to_nwb/Buzsaki/convert_neuroscope.py
@@ -8,7 +8,7 @@ import to_nwb.neuroscope as ns
 
 session_path = '/Users/bendichter/Desktop/Buzsaki/data/buzsakilab.nyumc.org/datasets/McKenzieS/camkii4/20160817'
 
-stub = False
+stub = True
 
 subject_path, session_id = os.path.split(session_path)
 subject_id = os.path.split(subject_path)[1]
@@ -26,13 +26,10 @@ nwbfile = NWBFile(session_description='session_description',
 
 nwbfile.subject = Subject(subject_id=subject_id, species='Mus musculus')
 
-nwbfile = ns.write_electrode_table(nwbfile, session_path)
-
-nwbfile = ns.write_lfp(nwbfile, session_path, stub=stub)
-
-nwbfile = ns.write_units(nwbfile, session_path)
-
-nwbfile = ns.write_events(nwbfile, session_path)
+ns.write_electrode_table(nwbfile, session_path)
+ns.write_lfp(nwbfile, session_path, stub=stub)
+ns.write_units(nwbfile, session_path)
+ns.write_events(nwbfile, session_path)
 
 out_fname = session_path
 if stub:
@@ -42,6 +39,7 @@ out_fname += '.nwb'
 with NWBHDF5IO(out_fname, 'w') as io:
     io.write(nwbfile)
 
+#  test read
 with NWBHDF5IO(out_fname, 'r') as io:
     io.read()
 

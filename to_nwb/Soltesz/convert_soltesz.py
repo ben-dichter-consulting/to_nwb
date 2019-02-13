899114ba23d1b4a34fa474909c12fcd53f2e1c07
bendichter <ben.dichter@gmail.com>
2019-02-13 15:19:28 -0800

    modularize convert_soltesz

--- to_nwb/Soltesz/convert_soltesz.py
+++ to_nwb/Soltesz/convert_soltesz.py
@@ -8,11 +8,13 @@ import numpy as np
 from pynwb import NWBFile, NWBHDF5IO
 from pynwb.behavior import SpatialSeries, Position
 
+import sys
+
 
 def read_ragged_array(struct, x):
 
     if 'Cell Index' in struct:
-        x = np.where(struct['Cell Index'][:]==x)[0]
+        x = np.where(struct['Cell Index'][:] == x)[0]
 
     x = int(x)
 
@@ -28,49 +30,58 @@ def get_neuroh5_cell_data(fpath):
         pops = f['Populations']
         for cell_type in pops:
             spike_struct = pops[cell_type]['Vector Stimulus 100']['spiketrain']
-            for i, pop_id in enumerate(spike_struct['Cell Index']):
-                spike_times = read_ragged_array(spike_struct, pop_id) / 100
+            for pop_id in spike_struct['Cell Index']:
+                spike_times = read_ragged_array(spike_struct, pop_id) / 1000
                 yield {'pop_id': int(pop_id), 'spike_times': spike_times, 'cell_type': cell_type}
 
 
-fpath = '/Users/bendichter/Desktop/Soltesz/data/DG_PP_spikes_101718.h5'
+def neuroh5_to_nwb(fpath='/Users/bendichter/Desktop/Soltesz/data/DG_PP_spikes_101718.h5', out_path=None):
+
+    if out_path is None:
+        out_path = fpath[:-3] + '.nwb'
+
+    fname = os.path.split(fpath)[1]
+    identifier = fname[:-4]
+
+    nwbfile = NWBFile(session_description='session_description',
+                      identifier=identifier,
+                      session_start_time=datetime.now().astimezone(),
+                      institution='Stanford University', lab='Soltesz')
+
+    # Position
+    trajectory = 'Trajectory 100'
+    with File(fpath, 'r') as f:
+        x = f[trajectory]['x']
+        y = f[trajectory]['y']
+        rate = 1 / (f[trajectory]['t'][1] - f[trajectory]['t'][0]) * 1000
+
+        pos_data = np.array([x, y]).T
+
+    behavior_mod = nwbfile.create_processing_module('behavior', 'holds behavior data')
+
+    spatial_series = SpatialSeries('Position', pos_data,
+                                   reference_frame='NA',
+                                   conversion=1 / 100.,
+                                   resolution=np.nan,
+                                   rate=rate)
+
+    behavior_mod.add_data_interface(Position(spatial_series))
+
+    nwbfile.add_unit_column('cell_type', 'cell type')
+    nwbfile.add_unit_column('pop_id', 'cell number within population')
+
+    for unit_dict in tqdm(get_neuroh5_cell_data(fpath),
+                          total=38000+34000,
+                          desc='reading cell data'):
+        nwbfile.add_unit(**unit_dict)
+
+    with NWBHDF5IO(out_path, 'w') as io:
+        io.write(nwbfile)
 
 
-fname = os.path.split(fpath)[1]
-identifier = fname[:-4]
-
-nwbfile = NWBFile(session_description='session_description',
-                  identifier='identifier',
-                  session_start_time=datetime.now().astimezone(),
-                  institution='Stanford', lab='Soltesz')
-
-# Position
-trajectory = 'Trajectory 100'
-with File(fpath, 'r') as f:
-    x = f[trajectory]['x']
-    y = f[trajectory]['y']
-    rate = 1 / (f[trajectory]['t'][1] - f[trajectory]['t'][0]) * 1000
-
-    pos_data = np.array([x, y]).T
-
-behavior_mod = nwbfile.create_processing_module('behavior', 'holds behavior data')
+def main(argv):
+    neuroh5_to_nwb('/Users/bendichter/Desktop/Soltesz/data/DG_PP_spikes_101718.h5')
 
 
-spatial_series = SpatialSeries('Position', pos_data,
-                               reference_frame='NA',
-                               conversion=1 / 100.,
-                               resolution=0.1,
-                               rate=rate)
-
-behavior_mod.add_data_interface(Position(spatial_series))
-
-nwbfile.add_unit_column('cell_type', 'cell type')
-nwbfile.add_unit_column('pop_id', 'cell number within population')
-for unit_dict in tqdm(get_neuroh5_cell_data(fpath),
-                      total=38000+34000,
-                      desc='reading cell data'):
-    nwbfile.add_unit(**unit_dict)
-
-out_path = fpath[:-3] + '.nwb'
-with NWBHDF5IO(out_path, 'w') as io:
-    io.write(nwbfile)
+if __name__ == "__main__":
+    main(sys.argv[1:])

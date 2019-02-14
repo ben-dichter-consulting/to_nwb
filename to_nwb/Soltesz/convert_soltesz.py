acdefabf30198eb7ce154066209e07854bc5781e
bendichter <ben.dichter@gmail.com>
2019-02-13 16:08:55 -0800

    modularize

--- to_nwb/Soltesz/convert_soltesz.py
+++ to_nwb/Soltesz/convert_soltesz.py
@@ -8,6 +8,8 @@ import numpy as np
 from pynwb import NWBFile, NWBHDF5IO
 from pynwb.behavior import SpatialSeries, Position
 
+from ..utils import check_module
+
 import sys
 
 
@@ -24,15 +26,29 @@ def read_ragged_array(struct, x):
     return struct['Attribute Value'][start:stop].astype(float)
 
 
-def get_neuroh5_cell_data(fpath):
+def get_neuroh5_cell_data(f):
 
-    with File(fpath, 'r') as f:
-        pops = f['Populations']
-        for cell_type in pops:
-            spike_struct = pops[cell_type]['Vector Stimulus 100']['spiketrain']
-            for pop_id in spike_struct['Cell Index']:
-                spike_times = read_ragged_array(spike_struct, pop_id) / 1000
-                yield {'pop_id': int(pop_id), 'spike_times': spike_times, 'cell_type': cell_type}
+    pops = f['Populations']
+    for cell_type in pops:
+        spike_struct = pops[cell_type]['Vector Stimulus 100']['spiketrain']
+        for pop_id in spike_struct['Cell Index']:
+            spike_times = read_ragged_array(spike_struct, pop_id) / 1000
+            yield {'pop_id': int(pop_id), 'spike_times': spike_times, 'cell_type': cell_type}
+
+
+def write_position(nwbfile, f, name='Trajectory 100'):
+    obj = f[name]
+    behavior_mod = check_module(nwbfile, 'behavior')
+
+    spatial_series = SpatialSeries('Position', data=np.array([obj['x'], obj['y']]).T,
+                                   reference_frame='NA',
+                                   conversion=1 / 100.,
+                                   resolution=np.nan,
+                                   rate=1 / np.diff(obj['t'][:2]) * 1000)
+
+    behavior_mod.add_data_interface(Position(spatial_series))
+
+    return behavior_mod
 
 
 def neuroh5_to_nwb(fpath='/Users/bendichter/Desktop/Soltesz/data/DG_PP_spikes_101718.h5', out_path=None):
@@ -48,32 +64,16 @@ def neuroh5_to_nwb(fpath='/Users/bendichter/Desktop/Soltesz/data/DG_PP_spikes_10
                       session_start_time=datetime.now().astimezone(),
                       institution='Stanford University', lab='Soltesz')
 
-    # Position
-    trajectory = 'Trajectory 100'
-    with File(fpath, 'r') as f:
-        x = f[trajectory]['x']
-        y = f[trajectory]['y']
-        rate = 1 / (f[trajectory]['t'][1] - f[trajectory]['t'][0]) * 1000
+    with File('fpath', 'r') as f:
+        write_position(nwbfile, f)
 
-        pos_data = np.array([x, y]).T
+        nwbfile.add_unit_column('cell_type', 'cell type')
+        nwbfile.add_unit_column('pop_id', 'cell number within population')
 
-    behavior_mod = nwbfile.create_processing_module('behavior', 'holds behavior data')
-
-    spatial_series = SpatialSeries('Position', pos_data,
-                                   reference_frame='NA',
-                                   conversion=1 / 100.,
-                                   resolution=np.nan,
-                                   rate=rate)
-
-    behavior_mod.add_data_interface(Position(spatial_series))
-
-    nwbfile.add_unit_column('cell_type', 'cell type')
-    nwbfile.add_unit_column('pop_id', 'cell number within population')
-
-    for unit_dict in tqdm(get_neuroh5_cell_data(fpath),
-                          total=38000+34000,
-                          desc='reading cell data'):
-        nwbfile.add_unit(**unit_dict)
+        for unit_dict in tqdm(get_neuroh5_cell_data(fpath),
+                              total=38000+34000,
+                              desc='reading cell data'):
+            nwbfile.add_unit(**unit_dict)
 
     with NWBHDF5IO(out_path, 'w') as io:
         io.write(nwbfile)

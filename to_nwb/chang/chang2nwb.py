68664b1c71de1f20eba3a05b4342373fc722b877
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2019-04-24 12:54:43 -0400

    handle missing labels

--- to_nwb/chang/chang2nwb.py
+++ to_nwb/chang/chang2nwb.py
@@ -603,6 +603,8 @@ def chang2nwb(blockpath, outpath=None, session_start_time=None,
                 df = make_df(parseout, 0, subject_id, align_pos=0)
             except:
                 df = pd.DataFrame(parseout)
+
+            df = df.loc[df['label'].astype('bool'), :]  # handle empty labels
             nwbfile.add_trial_column(
                 'speak', 'if True, subject is speaking. If False, subject is listening')
             nwbfile.add_trial_column('condition', 'syllable spoken')
